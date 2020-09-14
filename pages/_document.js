bfaf050205b2f0132b1e190fe3daf53cdf30e6b8
jeremiemk <jeremieemk@gmail.com>
2020-09-14 12:25:49 +0100

    test google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -23,11 +23,14 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>My page</title>
-          <script
+          {/* <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=GTM-5F7DK2H"
+          /> */}
+          {/* <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-115969774-3"
-          />
+          /> */}
           <script
             dangerouslySetInnerHTML={{
               __html: `<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115969774-3"></script>
@@ -40,22 +43,44 @@ export default class MyDocument extends Document {
             }}
           />
           <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=GTM-5F7DK2H`}
+          />
+          <script
             dangerouslySetInnerHTML={{
-              __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-5F7DK2H');</script>`,
+              __html: `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5F7DK2H');</script>
+<!-- End Google Tag Manager -->`,
             }}
           />
-          {/* Step 5: Output the styles in the head  */}
+
+          {/* <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-5F7DK2H', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          /> */}
+
+          {/*  Output the styles in the head  */}
           {this.props.styleTags}
         </Head>
         <body>
           <script
             dangerouslySetInnerHTML={{
-              __html: `<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F7DK2H"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+              __html: `<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F7DK2H"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`,
             }}
           />
           <Main />
